963609f68a052c97df34094ef428377e8c140b5b
Roshan <rc0747507@gmail.com>
2026-06-25 11:26:29 +0530

    DAY 7 DONE

---
+++ Week 3/DAY-7/Problem-2.cpp
@@ -0,0 +1,26 @@
+                       //Write a program to Reverse a string.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    char str[100];
+
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+
+    int length = 0;
+
+    while(str[length] != '\0') {
+        length++; }
+
+    for(int i = 0, j = length - 1; i < j; i++, j--) {
+        char temp = str[i];
+        str[i] = str[j];
+        str[j] = temp; }
+
+    cout << "Reversed String = " << str;
+
+    return 0;
+}
